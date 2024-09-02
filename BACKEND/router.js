1c58bba96e40a5086b85fbfacb96c96565d7e23a
kushkapadia <kush2004kapadia@gmail.com>
2024-09-02 17:51:34 +0530

    CRD routes

--- BACKEND/router.js
+++ BACKEND/router.js
@@ -1,39 +1,77 @@
-
-const express = require('express');
-const router = express.Router();
-const AuthHelper = require('./helper/JWTAuthHelper');
-const TryCatch = require('./helper/TryCatch');
-const Messages = require('./constants/Messages');
+const express = require("express")
+const router = express.Router()
+const AuthHelper = require("./helper/JWTAuthHelper")
+const TryCatch = require("./helper/TryCatch")
+const Messages = require("./constants/Messages")
 
 //imports here
-const projectController = require('./controllers/projectController');
+const questionController = require("./controllers/questionController")
 
-const studentController = require('./controllers/studentController');
+const internshipapplicationController = require("./controllers/internshipapplicationController")
 
+const jobpostController = require("./controllers/jobpostController")
+
+const chatController = require("./controllers/chatController")
+const projectController = require("./controllers/projectController")
+
+const studentController = require("./controllers/studentController")
 
 //code here
+
+//Entity - Question --start
+//Authentication - Question
+router.post("/register-question", new TryCatch(questionController.apiRegister).tryCatchGlobe())
+router.post("/login-question", new TryCatch(questionController.apiLogin).tryCatchGlobe())
+
+//CRUD Operations - Question
+router.post("/question/does-email-exists", AuthHelper.verifyToken, new TryCatch(questionController.doesEmailExist).tryCatchGlobe())
+router.get("/question/get-by-id/:id", AuthHelper.verifyToken, new TryCatch(questionController.getById).tryCatchGlobe())
+router.get("/question/get-by-email/:email", AuthHelper.verifyToken, new TryCatch(questionController.getByEmail).tryCatchGlobe())
+router.get("/question/get-all", AuthHelper.verifyToken, new TryCatch(questionController.getAllQuestions).tryCatchGlobe())
+router.delete("/question/delete-by-id/:id", AuthHelper.verifyToken, new TryCatch(questionController.deleteById).tryCatchGlobe())
+//Entity - Question - End
+
+//Entity - InternshipApplication --start
+
+//CRUD Operations - InternshipApplication
+router.post("/internshipapplication/create", AuthHelper.verifyToken, new TryCatch(internshipapplicationController.createInternshipApplication).tryCatchGlobe())
+router.get("/internshipapplication/get-by-id/:id", AuthHelper.verifyToken, new TryCatch(internshipapplicationController.getById).tryCatchGlobe())
+router.get("/internshipapplication/get-all", AuthHelper.verifyToken, new TryCatch(internshipapplicationController.getAllInternshipApplications).tryCatchGlobe())
+router.delete("/internshipapplication/delete-by-id/:id", AuthHelper.verifyToken, new TryCatch(internshipapplicationController.deleteById).tryCatchGlobe())
+//Entity - InternshipApplication - End
+
+//Entity - JobPost --start
+
+//CRUD Operations - JobPost
+router.post("/jobpost/create", AuthHelper.verifyToken, new TryCatch(jobpostController.createJobPost).tryCatchGlobe())
+router.get("/jobpost/get-by-id/:id", AuthHelper.verifyToken, new TryCatch(jobpostController.getById).tryCatchGlobe())
+router.get("/jobpost/get-all", AuthHelper.verifyToken, new TryCatch(jobpostController.getAllJobPosts).tryCatchGlobe())
+router.delete("/jobpost/delete-by-id/:id", AuthHelper.verifyToken, new TryCatch(jobpostController.deleteById).tryCatchGlobe())
+//Entity - JobPost - End
+
+router.post("/send-chat", AuthHelper.verifyToken, new TryCatch(chatController.sendChat).tryCatchGlobe())
+router.get("/get-my-chat/:id/:chatContactId", AuthHelper.verifyToken, new TryCatch(chatController.getChatConvo).tryCatchGlobe())
 
 //Entity - Project --start
 
 //CRUD Operations - Project
-router.post('/project/create', AuthHelper.verifyToken, new TryCatch(projectController.createProject).tryCatchGlobe());
-router.get('/project/get-by-id/:id', AuthHelper.verifyToken, new TryCatch(projectController.getById).tryCatchGlobe());
-router.get('/project/get-all', AuthHelper.verifyToken, new TryCatch(projectController.getAllProjects).tryCatchGlobe());
-router.delete('/project/delete-by-id/:id', AuthHelper.verifyToken, new TryCatch(projectController.deleteById).tryCatchGlobe());
+router.post("/project/create", AuthHelper.verifyToken, new TryCatch(projectController.createProject).tryCatchGlobe())
+router.get("/project/get-by-id/:id", AuthHelper.verifyToken, new TryCatch(projectController.getById).tryCatchGlobe())
+router.get("/project/get-all", AuthHelper.verifyToken, new TryCatch(projectController.getAllProjects).tryCatchGlobe())
+router.delete("/project/delete-by-id/:id", AuthHelper.verifyToken, new TryCatch(projectController.deleteById).tryCatchGlobe())
 //Entity - Project - End
-
 
 //Entity - Student --start
 //Authentication - Student
-router.post('/register-student', new TryCatch(studentController.apiRegister).tryCatchGlobe());
-router.post('/login-student', new TryCatch(studentController.apiLogin).tryCatchGlobe());
+router.post("/register-student", new TryCatch(studentController.apiRegister).tryCatchGlobe())
+router.post("/login-student", new TryCatch(studentController.apiLogin).tryCatchGlobe())
 
 //CRUD Operations - Student
-router.post('/student/does-email-exists', AuthHelper.verifyToken, new TryCatch(studentController.doesEmailExist).tryCatchGlobe());
-router.get('/student/get-by-id/:id', AuthHelper.verifyToken, new TryCatch(studentController.getById).tryCatchGlobe());
-router.get('/student/get-by-email/:email', AuthHelper.verifyToken, new TryCatch(studentController.getByEmail).tryCatchGlobe());
-router.get('/student/get-all', AuthHelper.verifyToken, new TryCatch(studentController.getAllStudents).tryCatchGlobe());
-router.delete('/student/delete-by-id/:id', AuthHelper.verifyToken, new TryCatch(studentController.deleteById).tryCatchGlobe());
+router.post("/student/does-email-exists", AuthHelper.verifyToken, new TryCatch(studentController.doesEmailExist).tryCatchGlobe())
+router.get("/student/get-by-id/:id", AuthHelper.verifyToken, new TryCatch(studentController.getById).tryCatchGlobe())
+router.get("/student/get-by-email/:email", AuthHelper.verifyToken, new TryCatch(studentController.getByEmail).tryCatchGlobe())
+router.get("/student/get-all", AuthHelper.verifyToken, new TryCatch(studentController.getAllStudents).tryCatchGlobe())
+router.delete("/student/delete-by-id/:id", AuthHelper.verifyToken, new TryCatch(studentController.deleteById).tryCatchGlobe())
 //Entity - Student - End
 
-module.exports = router;
+module.exports = router
