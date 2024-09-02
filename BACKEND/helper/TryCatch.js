ffd4888240799de4c5302c187d85e542fa8d5c54
kushkapadia <kush2004kapadia@gmail.com>
2024-09-03 01:21:40 +0530

    misc

--- BACKEND/helper/TryCatch.js
+++ BACKEND/helper/TryCatch.js
@@ -1,28 +1,30 @@
+const JsonResponse = require("./JsonResponse")
 
-const JsonResponse = require('./JsonResponse')
-
-let  TryCatch = function(handler){
-this.handler = handler
+let TryCatch = function (handler) {
+  this.handler = handler
 }
-    /**
-     * tryCatchGlobe
-     */
-    TryCatch.prototype.tryCatchGlobe = function()  {
-        return async (req, res, next) => {
-            try {
-             await this.handler(req,res);   
-            } catch (error) {
-                res.locals.data = {
-                    isVaild:false
-                };
-                res.locals.message = error;
-                // console.log(error);
-            //   new JsonResponse.jsonError(req, res);
-            new JsonResponse(req, res).jsonError();
+/**
+ * tryCatchGlobe
+ */
+TryCatch.prototype.tryCatchGlobe = function () {
+  return async (req, res, next) => {
+    try {
+      console.log("try")
 
-                next(error);
-            }
-        }
+      await this.handler(req, res)
+    } catch (error) {
+      console.log("catch")
+      res.locals.data = {
+        isVaild: false
+      }
+      res.locals.message = error
+      // console.log(error);
+      //   new JsonResponse.jsonError(req, res);
+      new JsonResponse(req, res).jsonError()
+
+      next(error)
     }
+  }
+}
 
-    module.exports = TryCatch
+module.exports = TryCatch
