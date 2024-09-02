fbde0ab8e0a1628f4276e88abe99a1d4ba1f7fdb
kushkapadia <kush2004kapadia@gmail.com>
2024-09-03 01:20:39 +0530

    enabled cors

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -1,28 +1,30 @@
-const express = require("express");
-const router = require("./router");
-const morgan = require("morgan");
+const express = require("express")
+const router = require("./router")
+const morgan = require("morgan")
 
-const cors = require("cors");
-
+const cors = require("cors")
 
 //imports here
-
 
 //code here
 
 // Initialize our server
-const app = express();
+const app = express()
 //To access the data user inputs in form.
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }))
 //just a bolierplate code, tells our express server to add the user submitted data to request object.
-app.use(express.json());
+app.use(express.json())
+app.use(
+  cors({
+    origin: "http://localhost:3000", // Replace with your frontend URL
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true
+  })
+)
+app.use(express.static("public"))
+app.use(morgan("dev"))
+app.use("/", router)
 
-app.use(express.static("public"));
-app.use(morgan("dev"));
-app.use("/", router);
+app.use(cors())
 
-app.use(cors());
-
-
-
-module.exports = app;
+module.exports = app
