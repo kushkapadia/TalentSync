c15080e00de016dcfaebc5dd9ffd2f4efe79ae42
kushkapadia <kush.salesbrahma@gmail.com>
2024-11-06 15:35:29 +0530

    misc

--- BACKEND/helper/JWTAuthHelper.js
+++ BACKEND/helper/JWTAuthHelper.js
@@ -1,31 +1,29 @@
-        
-const jwt = require("jsonwebtoken");
-const JsonResponse = require('./JsonResponse');
+const jwt = require("jsonwebtoken")
+const JsonResponse = require("./JsonResponse")
 
 exports.verifyToken = function (req, res, next) {
-  try{
-    const bearerToken = req.headers["authorization"];
+  try {
+    const bearerToken = req.headers["authorization"]
 
-    const bearer = bearerToken.split(" ");
+    const bearer = bearerToken.split(" ")
 
-    const token = bearer[1];
+    const token = bearer[1]
 
-    console.log(token);
+    console.log(token)
 
-    req.apiUser = jwt.verify(token, process.env.JWTSECRET);
-    console.log(req.apiUser)  
+    req.apiUser = jwt.verify(token, process.env.JWTSECRET)
+    console.log(req.apiUser)
     next()
-  } catch(error){
-console.log("here")
+  } catch (error) {
+    console.log("here")
     res.locals.data = {
-        isVaild: false, 
-        authorizationFailed: true,
-      };
+      isVaild: false,
+      authorizationFailed: true
+    }
 
-      res.locals.message = error;
-      new JsonResponse(req, res).jsonError();
-      // next(error)
-      // return true
+    res.locals.message = error
+    new JsonResponse(req, res).jsonError()
+    // next(error)
+    // return true
   }
-};
-        
+}
