c1e8de6299d9d5ee12869063fcca79a9eba23119
Sounak Nandi <sounaknandi24@gmail.com>
2024-11-06 19:00:37 +0530

    Upload Intenships file created

---
+++ frontendNew/app/pages/UploadInternships.js
@@ -0,0 +1,13 @@
+// pages/UploadInternships.js
+import React from "react"
+
+function UploadInternships() {
+  return (
+    <div>
+      <h2>Upload Internships</h2>
+      {/* Here you can add the internship upload form or relevant functionality */}
+    </div>
+  )
+}
+
+export default UploadInternships
