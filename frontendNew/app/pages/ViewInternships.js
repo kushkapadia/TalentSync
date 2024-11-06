402b9bc275eea1f254401583b3cd974f3bda4714
Sounak Nandi <sounaknandi24@gmail.com>
2024-11-06 19:00:49 +0530

    View Internships file created

---
+++ frontendNew/app/pages/ViewInternships.js
@@ -0,0 +1,13 @@
+// pages/ViewInternships.js
+import React from "react"
+
+function ViewInternships() {
+  return (
+    <div>
+      <h2>View Internships</h2>
+      {/* Add logic to view listed internships */}
+    </div>
+  )
+}
+
+export default ViewInternships
