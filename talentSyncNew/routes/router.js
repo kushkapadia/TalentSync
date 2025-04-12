a987fe0c24fe47c01c8a2351b8cc3d3e21933ced
Sounak Nandi <sounaknandi24@gmail.com>
2025-04-12 22:57:43 +0530

    Admin profile view fixed

--- talentSyncNew/routes/router.js
+++ talentSyncNew/routes/router.js
@@ -179,5 +179,6 @@ router.get("/mentor-profile/:id", new TryCatch(mentorController.displayMentorPro
 router.get('/admin-active-internships', new TryCatch(adminController.displayActiveInternshipPage).tryCatchGlobe())
 router.get('/admin-view-internship/:id', new TryCatch(jobpostController.displayAdminInternshipDetailPage).tryCatchGlobe())
 router.get('/admin-upload-internship', new TryCatch(jobpostController.displayAdminUploadInternshipPage).tryCatchGlobe())
+router.get("/admin-profile/:id", new TryCatch(adminController.displayAdminProfile).tryCatchGlobe())
 
 module.exports = router
